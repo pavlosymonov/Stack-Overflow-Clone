da3e54c735964690e4496d295ee71e9eb62cef8b
Paul <simonov.pasha9822@gmail.com>
2020-10-19 18:54:58 +0300

    Updates

---
+++ precache-manifest.6545f6ef16fe0cf1b81f927f7e23ea3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5959a355732dc1fd34e110e38b938bcc",
+    "url": "/Stack-Overflow-Clone/index.html"
+  },
+  {
+    "revision": "7fd562246707036ca2b0",
+    "url": "/Stack-Overflow-Clone/static/css/main.05640100.chunk.css"
+  },
+  {
+    "revision": "182b7434d26970e9640b",
+    "url": "/Stack-Overflow-Clone/static/js/2.6a8ea22d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Stack-Overflow-Clone/static/js/2.6a8ea22d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fd562246707036ca2b0",
+    "url": "/Stack-Overflow-Clone/static/js/main.f7f74926.chunk.js"
+  },
+  {
+    "revision": "ab407ae88ff357eae198",
+    "url": "/Stack-Overflow-Clone/static/js/runtime-main.be51722f.js"
+  },
+  {
+    "revision": "5ea02efb7ba670db2d88936a5d48b237",
+    "url": "/Stack-Overflow-Clone/static/media/logo-stackoverflow.5ea02efb.svg"
+  }
+]);
