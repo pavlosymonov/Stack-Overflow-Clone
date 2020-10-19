a278dfd6d07a6cd6742931a5f220d978c4f4adc4
Paul <simonov.pasha9822@gmail.com>
2020-10-19 19:34:58 +0300

    Updates

---
+++ precache-manifest.6920626515e8bf46708897ccc346152e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e27a7ff3a6eb201483595a89b5eb631",
+    "url": "/Stack-Overflow-Clone/index.html"
+  },
+  {
+    "revision": "bacac2d7a24bd4f64b41",
+    "url": "/Stack-Overflow-Clone/static/css/main.f6252b10.chunk.css"
+  },
+  {
+    "revision": "b0fcba0919410aa815e5",
+    "url": "/Stack-Overflow-Clone/static/js/2.f824650d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Stack-Overflow-Clone/static/js/2.f824650d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bacac2d7a24bd4f64b41",
+    "url": "/Stack-Overflow-Clone/static/js/main.df5424de.chunk.js"
+  },
+  {
+    "revision": "ab407ae88ff357eae198",
+    "url": "/Stack-Overflow-Clone/static/js/runtime-main.be51722f.js"
+  },
+  {
+    "revision": "5ea02efb7ba670db2d88936a5d48b237",
+    "url": "/Stack-Overflow-Clone/static/media/logo-stackoverflow.5ea02efb.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.47422769d7287560c24cc83773405c34.js"
+  "/Stack-Overflow-Clone/precache-manifest.6920626515e8bf46708897ccc346152e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Stack-Overflow-Clone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
