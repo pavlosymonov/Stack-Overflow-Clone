f739d6d204e41950305b5fde7844f9b7c09ed039
Paul <simonov.pasha9822@gmail.com>
2020-10-19 19:32:50 +0300

    Updates

---
+++ precache-manifest.47422769d7287560c24cc83773405c34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24cec27038bdb806dc41f0c55be87f2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bacac2d7a24bd4f64b41",
+    "url": "/static/css/main.f6252b10.chunk.css"
+  },
+  {
+    "revision": "b0fcba0919410aa815e5",
+    "url": "/static/js/2.f824650d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.f824650d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bacac2d7a24bd4f64b41",
+    "url": "/static/js/main.df5424de.chunk.js"
+  },
+  {
+    "revision": "3076b037e6276ad236a1",
+    "url": "/static/js/runtime-main.bf4bdaea.js"
+  },
+  {
+    "revision": "5ea02efb7ba670db2d88936a5d48b237",
+    "url": "/static/media/logo-stackoverflow.5ea02efb.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Stack-Overflow-Clone/precache-manifest.2388c790855717e8c1d9e6ffd02304ef.js"
+  "/precache-manifest.47422769d7287560c24cc83773405c34.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Stack-Overflow-Clone/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
